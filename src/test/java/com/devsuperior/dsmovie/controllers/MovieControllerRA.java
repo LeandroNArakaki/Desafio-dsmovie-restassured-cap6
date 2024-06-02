ae4c3e15b994f2d7d5d38ad576b2c7eb23441742
Leandro Arakaki <leanarakaki@gmail.com>
2024-06-02 17:56:14 -0400

    Implementação das classes MovieControllerRA e ScoreControllerRA

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,35 +1,155 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class MovieControllerRA {
-	
+
+	private Long existingMovie, nonExistingMovie;
+	private String adminToken, clientToken, invalidToken;
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private Map<String, Object> postMovie;
+	private String title;
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+		baseURI = "http://localhost:8080";
+
+		existingMovie = 1L;
+		nonExistingMovie = 100L;
+
+		adminUsername = "maria@gmail.com";
+		adminPassword = "123456";
+
+		clientUsername = "alex@gmail.com";
+		clientPassword = "123456";
+
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		invalidToken = adminToken + "invalid";
+
+		title = "The Witcher";
+
+		postMovie = new HashMap<>();
+		postMovie.put("id", existingMovie);
+		postMovie.put("title", title);
+		postMovie.put("score", 4.5F);
+		postMovie.put("count", 3);
+		postMovie.put("image", "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
+
+
+	}
+
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+				.get("/movies")
+				.then()
+				.statusCode(200);
+
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+				.get("/movies?title={title}", title)
+				.then()
+				.statusCode(200)
+				.body("content.id[0]", is(1))
+				.body("content.title[0]", equalTo("The Witcher"))
+				.body("content.score[0]", is( 4.83F))
+				.body("content.count[0]", is( 3))
+				.body("content.image[0]", is("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		existingMovie = 2L;
+
+		given()
+				.get("/movies/{id}", existingMovie)
+				.then()
+				.statusCode(200)
+				.body("id", is(2))
+				.body("title", equalTo("Venom: Tempo de Carnificina"))
+				.body("score", is( 3.3F))
+				.body("count", is( 3))
+				.body("image", is("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+		given()
+				.get("/movies/{id}", nonExistingMovie)
+				.then()
+				.statusCode(404);
+
 	}
 	
 	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+		postMovie.put("title", null);
+		JSONObject newMovie = new JSONObject(postMovie);
+
+		given()
+				.port(port)
+				.header("Content-Type", "application/json")
+				.header("Authorization", "Bearer " + adminToken)
+				.body(newMovie)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.post("/movies")
+				.then()
+				.statusCode(422)
+				.body("errors.message[0]", equalTo("Campo requerido"));
 	}
 	
 	@Test
 	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovie);
+
+		given()
+				.port(port)
+				.header("Content-Type", "application/json")
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newMovie)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.post("/movies")
+				.then()
+				.statusCode(403);
+
 	}
 	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovie);
+
+		given()
+				.port(port)
+				.header("Content-Type", "application/json")
+				.header("Authorization", "Bearer " + invalidToken)
+				.body(newMovie)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.post("/movies")
+				.then()
+				.statusCode(401);
+
 	}
 }
--- src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,18 +1,101 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class ScoreControllerRA {
-	
-	@Test
-	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
-	}
-	
-	@Test
-	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
-	}
-	
-	@Test
-	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
-	}
+
+    private Double existingScore, nonExistingScore;
+    private Long existingMovie, nonExistingMovie;
+    private String adminToken, clientToken, invalidToken;
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private Map<String, Object> postScore;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        baseURI = "http://localhost:8080";
+
+        existingScore = 4.5;
+        nonExistingScore = 10.0;
+
+        existingMovie = 1L;
+        nonExistingMovie = 100L;
+
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+
+        clientUsername = "alex@gmail.com";
+        clientPassword = "123456";
+
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        invalidToken = adminToken + "invalid";
+
+        postScore = new HashMap<>();
+        postScore.put("movieId", existingMovie);
+        postScore.put("score", existingScore);
+
+    }
+
+    @Test
+    public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+        postScore.put("movieId", nonExistingMovie);
+        JSONObject newScore = new JSONObject(postScore);
+
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newScore)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+        postScore.put("movieId", null);
+        JSONObject newScore = new JSONObject(postScore);
+
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newScore)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(422);
+    }
+
+    @Test
+    public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+        postScore.put("score", -1.0);
+        JSONObject newScore = new JSONObject(postScore);
+
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newScore)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .put("/scores")
+                .then()
+                .statusCode(422);
+    }
 }
